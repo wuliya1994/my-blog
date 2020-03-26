2047ca57f408c2787fd4172e081b02c24eca4888
centingshuang <you@example.com>
2020-03-26 15:03:28 +0800

    更新Dockerfile 配置

--- updateDocker.sh
+++ updateDocker.sh
@@ -1,3 +1,5 @@
 git pull && \
 docker build -t isuwang-blog -f ./docker/Dockerfile . && \
+docker stop isuwang-blog && \
+docker rm isuwang-blog && \
 docker run -p 4000:4000 --name isuwang-blog -d  isuwang-blog
